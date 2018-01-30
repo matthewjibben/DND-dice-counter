fd44f4dbd8311adae5b7b1a6ec6f4fd000aca0e9
Matthew <matthew.jibben@gmail.com>
2018-01-30 13:37:11 -0700

    removed unused code

--- dice counter.py
+++ dice counter.py
@@ -1,14 +1,3 @@
-import numpy
-import matplotlib.pyplot as plt
-import math
-import tkinter
-
-
-def addToCount(dict, val):
-    if val in dict:
-        dict[val] += 1
-    else:
-        dict[val] = 1
 # get user input for these variables
 sideCount = 6
 diceCount = 2
